d315fca5f5022fd57a898fefd69ca7e6ceca9728
KenniBlank <keniblank@gmail.com>
2024-08-14 13:44:20 +0545

    some control update

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 const toTop = () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
 };
+
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+});
+document.addEventListener('contextmenu', event => event.preventDefault())
